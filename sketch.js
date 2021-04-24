f8b23c16e7b59c52132e4de1ce5a78f5c2fcb80e
Aryan Rajib Borah <76152166+AryanBorah280107@users.noreply.github.com>
2021-04-24 13:54:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,9 +36,11 @@ function draw() {
     if(cat.x - mouse.x < (cat.width - mouse.width)/2){
         mouse.addAnimation("mouseStanding", mouseImage3);
         mouse.changeAnimation("mouseStanding");
+
         cat.addAnimation("catStop", catImage3);
         cat.changeAnimation("catStop");
         cat.velocityX = 0;
+        cat.x = 150;
 
     }
     
@@ -50,18 +52,17 @@ function keyPressed()   {
 
     //For moving and changing the animation write the code here
 
-    if(keyCode === LEFT_ARROW){
+    if(keyCode === LEFT_ARROW)  {
         mouse.addAnimation("mouseTeasing", mouseImage2);
         mouse.changeAnimation("mouseTeasing");
-        mouse.frameDalay = 25;
+        mouse.frameDelay = 20;
 
-    }
-
-    if(keyCode === LEFT_ARROW){
         cat.addAnimation("catRunning", catImage2);
         cat.changeAnimation("catRunning");
         cat.velocityX = -3;
+
     }
+
 }
 
 
